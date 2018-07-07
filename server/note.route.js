eef2477d16b45ba02550720f353fbdcf6bb93fd0
sysh333 <sysh324@gmail.com>
2018-07-07 15:28:53 +0900

    express note.route.js use ?

--- server/note.route.js
+++ server/note.route.js
@@ -5,9 +5,10 @@ const router = express.Router();
 
 router.get('/', async (req, res, next) => {
   let connection;
+  const sound_id = 1;
   try {
     connection = await db.getConnection();
-    const [rows] = await connection.query('select id, text, submit_time from note WHERE sound_id=1 ORDER BY submit_time');
+    const [rows] = await connection.query('select id, text, submit_time from `note` WHERE `sound_id` = ? ORDER BY submit_time',[sound_id]);
     res.json(rows);
   } catch (err) {
      console.log('*** catch ***',err); //クエリをエラーにしてコメントを外すと出力される
