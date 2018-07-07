687c92673be4a0b3f0d24d8595737b36f176bccb
sysh333 <sysh324@gmail.com>
2018-07-07 12:48:39 +0900

    api-check

--- server/note.route.js
+++ server/note.route.js
@@ -10,11 +10,12 @@ router.get('/', async (req, res, next) => {
     connection = await db.getConnection();
     // 接続に対してクエリを発行する
     // see: https://github.com/sidorares/node-mysql2
-    const [rows/* , fields */] = await connection.query('select id, text, submit_time, sound_id from note');
+    //const [rows/* , fields */] = await connection.query('select id, text, submit_time, sound_id from note');
+    const [rows/* , fields */] = await connection.query('select * from note');
     // 戻り値をjsonとしてレスポンスを返す
     res.json(rows);
   } catch (err) {
-    // console.log('*** catch ***'); クエリをエラーにしてコメントを外すと出力される
+     console.log('*** catch ***',err); //クエリをエラーにしてコメントを外すと出力される
     next(err);
   } finally {
     // console.log('*** finally ***'); コメントを外すと処理の成否に関わらず出力される
